a7321c35cc3be632189cad1358d3317bfeb6d2c1
luodongyang <luodongyang@meituan.com>
2019-12-14 01:20:48 +0800

    chore: add component list

---
+++ src/app/modules/build-up/component-list/component-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'byp-component-list',
+  templateUrl: './component-list.component.html',
+  styleUrls: ['./component-list.component.less']
+})
+export class ComponentListComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  /* handlers */
+  handleClick($event) {
+    console.log($event);
+    this.router.navigate(['../create'], { relativeTo: this.route});
+  }
+}
