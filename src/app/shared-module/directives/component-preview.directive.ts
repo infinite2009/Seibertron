87627efecb87a672f28e4e6210328ab2e5683115
luodongyang <luodongyang@meituan.com>
2020-01-12 23:50:06 +0800

    chore: add on init

--- src/app/shared-module/directives/component-preview.directive.ts
+++ src/app/shared-module/directives/component-preview.directive.ts
@@ -1,16 +1,20 @@
-import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
 import WidgetSchema from '../../interfaces/widget.schematics';
 
 @Directive({
   selector: '[bypComponentPreview]'
 })
-export class ComponentPreviewDirective {
+export class ComponentPreviewDirective implements OnInit {
 
   constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {
   }
 
   @Input()
   set schema(schema: WidgetSchema) {
-    
+
+  }
+
+  ngOnInit(): void {
+    console.log('component preview directive initialized');
   }
 }
