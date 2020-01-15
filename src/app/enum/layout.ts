519a48dd1ebe7911e1b334919442a351d4de2698
luodongyang <luodongyang@meituan.com>
2020-01-16 00:23:40 +0800

    refactor: remove some layout which should be classified as positioning

--- src/app/enum/layout.ts
+++ src/app/enum/layout.ts
@@ -1,16 +1,12 @@
 enum Layout {
-  // 列布局
+  // 列布局，一行一个
   column,
+  // 行布局，一行 n 个，超出换行
+  row,
   // 弹性布局,剩余空间会被控件填充
   flex,
   // 流式布局，剩余空间不会控件填充
   flow,
-  // 相对布局，将对于某个控件便宜
-  relative,
-  // 固定定位，相对于屏幕而言，也就是fixed
-  fixed,
-  // 吸顶定位，滚动到屏幕顶端就会吸顶
-  sticky
 }
 
 export default Layout;
