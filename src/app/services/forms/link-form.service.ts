6c21bd7331af9a967c7b23a4c3bc0e5080c27aff
luodongyang <luodongyang@meituan.com>
2020-04-09 22:47:42 +0800

    chore: extend link form service

--- src/app/services/forms/link-form.service.ts
+++ src/app/services/forms/link-form.service.ts
@@ -5,11 +5,12 @@ import LinkTarget from '@/enum/schema/link-target.enum';
 import ControlType from '@/enum/control-type.enum';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LinkFormService {
 
-  constructor() { }
+  constructor() {
+  }
 
   getLinkFormItems() {
     return [
@@ -59,6 +60,54 @@ export class LinkFormService {
           },
         ],
       } as IFormItem<string>),
+      new FormItem<number>({
+        name: 'fontSize',
+        label: '字号',
+        description: '字号',
+        value: 12,
+        controlType: ControlType.number,
+        unit: 'px',
+        required: false,
+      } as IFormItem<number>),
+      new FormItem<string>({
+        name: 'fontFamily',
+        label: '字体',
+        description: '字体',
+        value: 'PingFang SC',
+        required: true,
+        controlType: ControlType.select,
+        selectOptions: [
+          {
+            name: '苹方SC',
+            value: 'PingFang SC',
+          },
+          {
+            name: '微软雅黑',
+            value: 'Microsoft YaHei',
+          },
+          {
+            name: 'Helvetica',
+            value: 'Helvetica',
+          },
+        ],
+      } as IFormItem<string>),
+      new FormItem<number>({
+        name: 'lineHeight',
+        label: '行高',
+        description: '行高',
+        value: 12,
+        unit: 'px',
+        required: true,
+        controlType: ControlType.number,
+      } as IFormItem<number>),
+      new FormItem<boolean>({
+        name: 'fontWeight',
+        label: '加粗',
+        description: '加粗',
+        value: false,
+        required: false,
+        controlType: ControlType.checkbox,
+      } as IFormItem<boolean>),
     ];
   }
 }
