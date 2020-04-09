de75387f926f863afd80d2444e2325d67bf4badb
luodongyang <luodongyang@meituan.com>
2020-04-09 23:26:12 +0800

    refactor: modify tree view schema

--- src/app/interfaces/schema/tree-view.schema.ts
+++ src/app/interfaces/schema/tree-view.schema.ts
@@ -1,8 +1,6 @@
 import { AbstractWidgetSchema } from '@/interfaces/schema/abstractWidgetSchema';
 
-export default interface TreeViewSchema extends AbstractWidgetSchema {
-  node: {
-    // 渲染节点的 widget 或 component 名字
-    type: string;
-  };
+export default interface TreeViewSchema {
+  node: AbstractWidgetSchema;
+  items: TreeViewSchema[];
 }
