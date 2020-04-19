018354256557f8cc66ecd78f3711ffabe31f7252
luodongyang <luodongyang@meituan.com>
2020-04-19 09:16:57 +0800

    refactor: use enum instead

--- src/app/models/form/style-form-item.ts
+++ src/app/models/form/style-form-item.ts
@@ -1,10 +1,11 @@
 import IStyleFormItem from '@/interfaces/form/style-form-item';
 import FormItem from '@/models/form/form-item';
+import StyleValueUnit from '@/enum/style-value-unit';
 
 export default class StyleFormItem<T> extends FormItem<T> {
   constructor(opt: IStyleFormItem<T>) {
     super(opt);
-    this.unit = opt.unit || '';
+    this.unit = opt.unit || StyleValueUnit.none;
   }
 
   unit: string;
