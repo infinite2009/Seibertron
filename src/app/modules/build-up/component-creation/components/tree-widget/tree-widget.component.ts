597c5d9a366eca3d9ae14f53cc4e9630bbabdb56
luodongyang <luodongyang@meituan.com>
2020-04-19 13:16:48 +0800

    chore: modify host binding style logic

--- src/app/modules/build-up/component-creation/components/tree-widget/tree-widget.component.ts
+++ src/app/modules/build-up/component-creation/components/tree-widget/tree-widget.component.ts
@@ -1,9 +1,9 @@
 import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import WidgetTreeNode from '@/interfaces/tree-node';
 import { BasicFormService } from '@/services/forms/basic-form.service';
-import { StyleCollectionSchema } from '@/interfaces/schema/style-collection.schema';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import Layout from '@/enum/layout';
+import WidgetType from '@/enum/schema/widget-type.enum';
 
 @Component({
   selector: 'byp-tree-widget',
@@ -27,11 +27,14 @@ export class TreeWidgetComponent implements OnInit {
   @HostBinding('style')
   get hostStyles(): SafeStyle {
     // TODO 用其他生命周期优化下
-    let styleStr = this.basicFormService.convertSchemaToStyleStr(this.data.schema);
-    if (this.parent && this.parent.schema.styles.display.value === Layout.flex) {
-      styleStr += 'flex-shrink: 0';
+    if (this.data.schema.type === WidgetType.container) {
+      let styleStr = this.basicFormService.convertSchemaToStyleStr(this.data.schema);
+      if (this.parent && this.parent.schema.styles.display.value === Layout.flex) {
+        styleStr += 'flex-shrink: 0';
+      }
+      return this.domSanitizer.bypassSecurityTrustStyle(styleStr);
     }
-    return this.domSanitizer.bypassSecurityTrustStyle(styleStr);
+    return this.domSanitizer.bypassSecurityTrustStyle('flex-shrink: 0');
   }
 
   get styles() {
