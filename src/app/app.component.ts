f5c18e4e4a6ee5a022f24c0979b4ebc06e67e615
luodongyang <luodongyang@theduapp.com>
2020-04-20 11:46:44 +0800

    refactor: rename app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'byp',
+  selector: 'seibertron-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
