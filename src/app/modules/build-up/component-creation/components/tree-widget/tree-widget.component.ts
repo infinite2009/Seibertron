c028dff6359667c00b36bc631fd47b2c73930571
luodongyang <luodongyang@meituan.com>
2020-05-30 13:55:14 +0800

    fix: list row layout error

--- src/app/modules/build-up/component-creation/components/tree-widget/tree-widget.component.ts
+++ src/app/modules/build-up/component-creation/components/tree-widget/tree-widget.component.ts
@@ -1,12 +1,13 @@
+import Layout from '@/enum/layout';
+import WidgetType from '@/enum/schema/widget-type.enum';
 import DynamicObject from '@/interfaces/dynamic-object';
 import ListItemOption from '@/interfaces/list-item-option';
+import WidgetTreeNode from '@/interfaces/tree-node';
 import { DataMappingService } from '@/services/data-mapping.service';
+import { BasicFormService } from '@/services/forms/basic-form.service';
 import { SchemaService } from '@/services/schema.service';
 import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
-import WidgetTreeNode from '@/interfaces/tree-node';
-import { BasicFormService } from '@/services/forms/basic-form.service';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
-import Layout from '@/enum/layout';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -38,7 +39,7 @@ export class TreeWidgetComponent {
   @HostBinding('style')
   get hostStyles(): SafeStyle {
     // TODO 用其他生命周期优化下
-    if (this.schemaService.canHaveChildren(this.data.schema.type)) {
+    if (this.data.schema.type === WidgetType.container) {
       let styleStr = this.basicFormService.convertSchemaToStyleStr(this.data.schema);
       if (this.parent && this.parent.schema.styles.display.value === Layout.flex) {
         styleStr += 'flex-shrink: 0';
