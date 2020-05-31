3581be4dd04291adb3be0cbb2443dbbeb316f3fc
luodongyang <luodongyang@meituan.com>
2020-06-01 00:00:08 +0800

    chore: rollback

--- src/app/interfaces/schema/state-schema.ts
+++ src/app/interfaces/schema/state-schema.ts
@@ -1,4 +1,5 @@
 import StateOperator from '@/enum/schema/state-operator.enum';
+import DynamicObject from '@/interfaces/dynamic-object';
 
 export default interface StateSchema {
   // 状态的名字，用来翻译为状态变量名，或者作为状态的注释
@@ -8,6 +9,6 @@ export default interface StateSchema {
   ) => any
     | {
         operator: StateOperator;
-        input: any[];
+        options: DynamicObject;
       };
 }
