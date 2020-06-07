d45fe767fb615df445754009f2805ed967aee623
luodongyang <luodongyang@meituan.com>
2020-06-07 14:17:40 +0800

    chore: remove useless code

--- src/app/modules/build-up/component-creation/components/commands/insert-command/insert-command.component.ts
+++ src/app/modules/build-up/component-creation/components/commands/insert-command/insert-command.component.ts
@@ -43,48 +43,6 @@ export class InsertCommandComponent implements OnInit {
     items: (FormItem<any> | StyleFormItem<any>)[];
   }[] = [];
 
-  options = [
-    {
-      value: 'zhejiang',
-      label: 'Zhejiang',
-      children: [
-        {
-          value: 'hangzhou',
-          label: 'Hangzhou',
-          children: [
-            {
-              value: 'xihu',
-              label: 'West Lake',
-              isLeaf: true
-            }
-          ]
-        },
-        {
-          value: 'ningbo',
-          label: 'Ningbo',
-          isLeaf: true
-        }
-      ]
-    },
-    {
-      value: 'jiangsu',
-      label: 'Jiangsu',
-      children: [
-        {
-          value: 'nanjing',
-          label: 'Nanjing',
-          children: [
-            {
-              value: 'zhonghuamen',
-              label: 'Zhong Hua Men',
-              isLeaf: true
-            }
-          ]
-        }
-      ]
-    }
-  ];
-
   validateForm: FormGroup;
 
   visible: boolean = false;
