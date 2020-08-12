4a8827281817f8ff69c3a678fed8b7ffadc658aa
luodongyang <luodongyang@theduapp.com>
2020-08-12 17:29:21 +0800

    chore: extend api

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,19 +1,96 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
+import PageSchema from '@/interfaces/schema/page.schema';
+import WidgetFamilySchema from '@/types/widget-family-schema';
+import StateSchema from '@/interfaces/schema/state.schema';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
+  // 需要传递一个初始值
+  private messageSource = new ReplaySubject<any>(3);
+  message = this.messageSource.asObservable();
+  private pageSchemaMsgSource = new ReplaySubject<PageSchema>(1);
+  pageSchemaMsg = this.pageSchemaMsgSource.asObservable();
+  private selectedWidgetSchemaMsgSource = new ReplaySubject<WidgetFamilySchema>(1);
+  selectedWidgetSchemaMsg = this.selectedWidgetSchemaMsgSource.asObservable();
+
+  private selectedStateSchemaMsgSource = new ReplaySubject<StateSchema>(1);
+  selectedStateSchemaMsg = this.selectedStateSchemaMsgSource.asObservable();
+
+  private selectedSchemaMsgSource = new ReplaySubject<any>(1);
+  selectedSchemaMsg = this.selectedSchemaMsgSource.asObservable();
 
   constructor() {}
 
-  // 需要传递一个初始值
-  private messageSource = new ReplaySubject<any>(3);
-
-  message = this.messageSource.asObservable();
-
   sendMessage(message: any) {
     this.messageSource.next(message);
+  }
+
+  /*
+   * 更新 pageSchema
+   */
+  updatePageSchema(pageSchema: PageSchema) {
+    this.pageSchemaMsgSource.next(pageSchema);
+  }
+
+  /*
+   * 清空画布时，重置 page schema
+   */
+  resetPageSchema(pageSchema: PageSchema) {
+    this.updatePageSchema(pageSchema);
+  }
+
+  /*
+   * 选择一个 widget
+   */
+  selectWidget(widgetSchema: WidgetFamilySchema) {
+    this.selectedSchemaMsgSource.next({
+      type: 'widget',
+      schema: widgetSchema,
+    });
+  }
+
+  /*
+   * 反选当前选中的 widget
+   */
+  unselectWidget() {
+    this.selectWidget(null);
+  }
+
+  /*
+   * 选择一个状态
+   */
+  selectState(stateSchema: StateSchema) {
+    this.selectedSchemaMsgSource.next({
+      type: 'state',
+      schema: stateSchema,
+    });
+  }
+
+  /*
+   * 反选一个状态
+   */
+  unselectState() {
+    this.selectWidget(null);
+  }
+
+  /*
+   * 选择一个路由查询
+   */
+  // TODO 需要明确类型
+  selectRouteQuery(query: any) {
+    this.selectedSchemaMsgSource.next({
+      type: 'query',
+      schema: query,
+    });
+  }
+
+  /*
+   * 反选一个路由查询
+   */
+  unselectRouteQuery() {
+    this.selectRouteQuery(null);
   }
 }
