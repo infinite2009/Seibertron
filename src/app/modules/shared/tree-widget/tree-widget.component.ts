dd9322ad6fc9ac9b7096ed8c02ed52d2708344f3
luodongyang <luodongyang@meituan.com>
2020-08-20 23:54:14 +0800

    chore: add selection style on tree widget

--- src/app/modules/shared/tree-widget/tree-widget.component.ts
+++ src/app/modules/shared/tree-widget/tree-widget.component.ts
@@ -4,16 +4,23 @@ import DynamicObject from '@/interfaces/dynamic-object';
 import ListItemOption from '@/interfaces/list-item-option';
 import MessagePayload from '@/interfaces/message-payload';
 import ComponentSchema, { EventSchemaCollection, StateSchemaCollection } from '@/interfaces/schema/component.schema';
+import PageSchema from '@/interfaces/schema/page.schema';
 import WidgetTreeNode from '@/interfaces/tree-node';
 import DataMappingService from '@/services/data-mapping.service';
 import BasicFormService from '@/services/forms/basic-form.service';
 import SchemaService from '@/services/schema.service';
-import { ChangeDetectionStrategy, Component, HostBinding, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding, HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import { MessageService } from '@/services/message.service';
 import { Subscription } from 'rxjs';
 import { NzMessageService } from 'ng-zorro-antd';
-
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,15 +28,51 @@ import { NzMessageService } from 'ng-zorro-antd';
   templateUrl: './tree-widget.component.html',
   styleUrls: ['./tree-widget.component.less'],
 })
-export class TreeWidgetComponent implements OnInit, OnDestroy, OnChanges {
+export class TreeWidgetComponent implements OnInit, OnDestroy {
   constructor(
     private basicFormService: BasicFormService,
     private dataMappingService: DataMappingService,
     private schemaService: SchemaService,
     private domSanitizer: DomSanitizer,
-    private messageService: MessageService,
+    private msgService: MessageService,
     private nzMessageService: NzMessageService,
   ) {}
+
+  @Input()
+  componentSchema: ComponentSchema;
+
+  @Input()
+  componentStates: DynamicObject;
+
+  @Input()
+  data: WidgetTreeNode;
+
+  eventHandlers: (() => {})[] = [];
+  // event schema 的副本引用，这里用它是为了找到里边的 stateName，然后用来调用 stateFunctions 里边对应的函数
+  events: EventSchemaCollection;
+
+  @Input()
+  listItemOption: ListItemOption;
+
+  pageSchema: PageSchema;
+  pageSchemaSubscription: Subscription;
+  // 父节点的 data（根元素的 parent 为 null)
+  @Input()
+  parent: WidgetTreeNode;
+
+  @Input()
+  props: DynamicObject;
+
+  selectedMaterialSubscription: Subscription;
+  selectedSchema: any;
+  stateCtx: DynamicObject;
+  // component 里边根据 event schema 生成的函数字典（仅供预览使用，所以不能把它放到 schema 里边去）
+  stateFunctions: {
+    [key: string]: () => {};
+  };
+  // state schema 的副本引用
+  states: StateSchemaCollection;
+  subscription: Subscription;
 
   @HostBinding('style')
   get hostStyles(): SafeStyle {
@@ -44,74 +87,36 @@ export class TreeWidgetComponent implements OnInit, OnDestroy, OnChanges {
     return this.domSanitizer.bypassSecurityTrustStyle('flex-shrink: 0');
   }
 
+  @HostBinding('class.widget-selected')
+  get selected() {
+    return this.selectedSchema && this.selectedSchema.id === this.widgetSchema?.id;
+  }
+
   get styles() {
     return this.basicFormService.convertSchemaToStyles(this.data.schema);
   }
 
-  @Input()
-  data: WidgetTreeNode;
+  get widgetSchema() {
+    return this.data?.schema;
+  }
 
-  @Input()
-  props: DynamicObject;
-
-  // 父节点的 data（根元素的 parent 为 null)
-  @Input()
-  parent: WidgetTreeNode;
-
-  @Input()
-  componentSchema: ComponentSchema;
-
-  @Input()
-  componentStates: DynamicObject;
-
-  @Input()
-  listItemOption: ListItemOption;
-
-  subscription: Subscription;
-
-  stateCtx: DynamicObject;
-
-  // state schema 的副本引用
-  states: StateSchemaCollection;
-
-  // component 里边根据 event schema 生成的函数字典（仅供预览使用，所以不能把它放到 schema 里边去）
-  stateFunctions: {
-    [key: string]: () => {}
-  };
-
-  // event schema 的副本引用，这里用它是为了找到里边的 stateName，然后用来调用 stateFunctions 里边对应的函数
-  events: EventSchemaCollection;
-
-  eventHandlers: (() => {})[] = [];
+  @HostListener('click')
+  handleSelectingMaterial() {
+    if (this.selectedSchema?.id === this.widgetSchema?.id) {
+      this.msgService.unselectWidget();
+    } else {
+      this.msgService.selectWidget(this.widgetSchema);
+    }
+  }
 
   handleClickEvent() {
     for (let i = 0, l = this.eventHandlers.length; i < l; i++) {
       const handler = this.eventHandlers[i];
       const result = handler.call(this, this.stateCtx);
-      this.messageService.sendMessage({
+      this.msgService.sendMessage({
         type: 'outputState',
         payload: result,
       });
-    }
-  }
-
-  ngOnInit() {
-    if (!this.listItemOption) {
-      return;
-    }
-    this.stateCtx = this.dataMappingService.output({
-      ref: this.listItemOption.listDataRef
-    }, this.props?.dataSourceSchema, this.listItemOption);
-    this.subscription = this.messageService.message.subscribe(this.handleMessage);
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('changes: ', changes);
-  }
-
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
     }
   }
 
@@ -140,19 +145,55 @@ export class TreeWidgetComponent implements OnInit, OnDestroy, OnChanges {
         if (effect) {
           const { states } = effect;
           if (states && states.length && sourceWidget.id === this.data.schema.id) {
-            states.forEach(stateName => {
+            states.forEach((stateName) => {
               if (this.states[stateName]) {
-                this.eventHandlers = this.eventHandlers.concat(Object.entries(this.stateFunctions).filter(([key]) => {
-                  return key === stateName;
-                }).map(item => item[1]));
+                this.eventHandlers = this.eventHandlers.concat(
+                  Object.entries(this.stateFunctions)
+                    .filter(([key]) => {
+                      return key === stateName;
+                    })
+                    .map((item) => item[1])
+                );
               } else {
-                this.nzMessageService.error(`不存在的事件名:${stateName}`)
+                this.nzMessageService.error(`不存在的事件名:${stateName}`);
               }
             });
           }
         }
       });
     }
+  };
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    if (this.pageSchemaSubscription) {
+      this.pageSchemaSubscription.unsubscribe();
+    }
+    if (this.selectedMaterialSubscription) {
+      this.selectedMaterialSubscription.unsubscribe();
+    }
+  }
+
+  ngOnInit() {
+    if (this.listItemOption) {
+      this.stateCtx = this.dataMappingService.output(
+        {
+          ref: this.listItemOption.listDataRef,
+        },
+        this.props?.dataSourceSchema,
+        this.listItemOption
+      );
+      return;
+    }
+    this.subscription = this.msgService.message.subscribe(this.handleMessage);
+    this.pageSchemaSubscription = this.msgService.pageSchemaMsg.subscribe((schema: PageSchema) => {
+      this.pageSchema = schema;
+    });
+    this.selectedMaterialSubscription = this.msgService.selectedSchemaMsg.subscribe((data: any) => {
+      this.selectedSchema = data;
+    });
   }
 
   output(key: string) {
