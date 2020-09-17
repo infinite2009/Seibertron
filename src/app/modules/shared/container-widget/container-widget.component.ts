f0616080037d96dc1dffe58239ac83c6e87001d5
luodongyang <luodongyang@meituan.com>
2020-09-18 01:04:27 +0800

    chore: remove tree widget and use container widget instead

--- src/app/modules/shared/container-widget/container-widget.component.ts
+++ src/app/modules/shared/container-widget/container-widget.component.ts
@@ -1,34 +1,244 @@
+import Layout from '@/enum/layout';
+import InsertType from '@/enum/schema/widget-type.enum';
+import DynamicObject from '@/interfaces/dynamic-object';
+import InsertInfo from '@/interfaces/insert-info';
 import ListItemOption from '@/interfaces/list-item-option';
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import ContainerSchema from '@/interfaces/schema/container.schema';
+import MessagePayload from '@/interfaces/message-payload';
+import ComponentSchema, { EventSchemaCollection, StateSchemaCollection } from '@/interfaces/schema/component.schema';
+import PageSchema from '@/interfaces/schema/page.schema';
+import WidgetTreeNode from '@/interfaces/tree-node';
+import DataMappingService from '@/services/data-mapping.service';
 import BasicFormService from '@/services/forms/basic-form.service';
+import { MessageService } from '@/services/message.service';
+import SchemaService from '@/services/schema.service';
+import { ChangeDetectionStrategy, Component, HostBinding, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { NzMessageService } from 'ng-zorro-antd';
+import { Subscription } from 'rxjs';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'seibertron-container-widget',
   templateUrl: './container-widget.component.html',
-  styleUrls: ['./container-widget.component.less']
+  styleUrls: ['./container-widget.component.less'],
 })
-export class ContainerWidgetComponent implements OnInit {
-
+export class ContainerWidgetComponent implements OnInit, OnDestroy {
   constructor(
-    private basicFormService: BasicFormService
-  ) { }
+    private basicFormService: BasicFormService,
+    private dataMappingService: DataMappingService,
+    private schemaService: SchemaService,
+    private domSanitizer: DomSanitizer,
+    private msgService: MessageService,
+    private nzMessageService: NzMessageService
+  ) {}
+
+  @HostBinding('style')
+  get hostStyles(): SafeStyle {
+    let styles: DynamicObject = {
+      'flex-shrink': 0,
+    };
+    // TODO 用其他生命周期优化下
+    if (this.data.schema.type === InsertType.container) {
+      styles = this.basicFormService.convertSchemaToStyles(this.data.schema);
+      if (this.parent && this.parent.schema.styles.display.value === Layout.flex) {
+        styles['flex-shrink'] = 0;
+      }
+      if (this.selected) {
+        styles.border = '2px dashed #396fff';
+        styles['background-color'] = 'rgb(167, 190, 250)';
+      }
+      if (this.isEmpty) {
+        styles.position = 'relative';
+      }
+    }
+    return this.domSanitizer.bypassSecurityTrustStyle(this.basicFormService.convertStyleDicToStr(styles));
+  }
+
+  @HostBinding('class.widget-empty')
+  get isEmpty(): boolean {
+    // 空容器
+    return this.widgetSchema.type === InsertType.container && !('children' in this.widgetSchema);
+  }
+
+  get selected() {
+    return this.selectedSchema && this.selectedSchema.id === this.widgetSchema?.id;
+  }
+
+  get widgetSchema() {
+    return this.data?.schema;
+  }
 
   @Input()
-  schema: ContainerSchema;
+  componentSchema: ComponentSchema;
 
   @Input()
-  parentSchema: ContainerSchema;
+  componentStates: DynamicObject;
+
+  @Input()
+  data: WidgetTreeNode;
+
+  eventHandlers: (() => {})[] = [];
+  // event schema 的副本引用，这里用它是为了找到里边的 stateName，然后用来调用 stateFunctions 里边对应的函数
+  events: EventSchemaCollection;
 
   @Input()
   listItemOption: ListItemOption;
 
-  get styles() {
-    return this.basicFormService.convertSchemaToStyles(this.schema);
+  pageSchema: PageSchema;
+  pageSchemaSubscription: Subscription;
+  // 父节点的 data（根元素的 parent 为 null)
+  @Input()
+  parent: WidgetTreeNode;
+
+  @Input()
+  props: DynamicObject;
+
+  selectedMaterialSubscription: Subscription;
+  selectedSchema: any;
+  stateCtx: DynamicObject;
+  // component 里边根据 event schema 生成的函数字典（仅供预览使用，所以不能把它放到 schema 里边去）
+  stateFunctions: {
+    [key: string]: () => {};
+  };
+  // state schema 的副本引用
+  states: StateSchemaCollection;
+  subscription: Subscription;
+
+  @HostListener('dndDrop', ['$event'])
+  onDrop($event: any) {
+    $event.event.preventDefault();
+    $event.event.stopImmediatePropagation();
+    const schema = this.schemaService.generateSchema($event.data.type);
+    this.insertMaterial({
+      type: $event.data.type,
+      data: schema,
+    });
+  }
+
+  @HostListener('click', ['$event'])
+  handleSelectingMaterial($event: Event) {
+    $event.stopImmediatePropagation();
+    if (this.selectedSchema?.id === this.widgetSchema?.id) {
+      this.msgService.unselectWidget();
+    } else {
+      this.msgService.selectWidget(this.widgetSchema);
+    }
+  }
+
+  getStyles(index: number) {
+    if (this.data?.children.length) {
+      return this.basicFormService.convertSchemaToStyles(this.data.children[index].schema);
+    }
+    return {};
+  }
+
+  handleClickEvent() {
+    for (let i = 0, l = this.eventHandlers.length; i < l; i++) {
+      const handler = this.eventHandlers[i];
+      const result = handler.call(this, this.stateCtx);
+      this.msgService.sendMessage({
+        type: 'outputState',
+        payload: result,
+      });
+    }
+  }
+
+  /*
+   * 处理接收到的广播消息
+   */
+  handleMessage = (msg: MessagePayload) => {
+    const { type, payload } = msg;
+    switch (type) {
+      case 'event':
+        this.events = payload;
+        break;
+      case 'state':
+        this.states = payload;
+        break;
+      case 'stateFunctions':
+        this.stateFunctions = payload;
+        break;
+      default:
+        break;
+    }
+    this.eventHandlers = [];
+    if (this.stateFunctions && this.states && this.events) {
+      Object.entries(this.events).forEach(([, eventSchema]) => {
+        const { effect, sourceWidget } = eventSchema;
+        if (effect) {
+          const { states } = effect;
+          if (states && states.length && sourceWidget.id === this.data.schema.id) {
+            states.forEach((stateName) => {
+              if (this.states[stateName]) {
+                this.eventHandlers = this.eventHandlers.concat(
+                  Object.entries(this.stateFunctions)
+                    .filter(([key]) => {
+                      return key === stateName;
+                    })
+                    .map((item) => item[1])
+                );
+              } else {
+                this.nzMessageService.error(`不存在的事件名:${stateName}`);
+              }
+            });
+          }
+        }
+      });
+    }
+  };
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    if (this.pageSchemaSubscription) {
+      this.pageSchemaSubscription.unsubscribe();
+    }
+    if (this.selectedMaterialSubscription) {
+      this.selectedMaterialSubscription.unsubscribe();
+    }
   }
 
   ngOnInit() {
+    if (this.listItemOption) {
+      this.stateCtx = this.dataMappingService.output(
+        {
+          ref: this.listItemOption.listDataRef,
+        },
+        this.props?.dataSourceSchema,
+        this.listItemOption
+      );
+      return;
+    }
+    this.subscription = this.msgService.message.subscribe(this.handleMessage);
+    this.pageSchemaSubscription = this.msgService.pageSchemaMsg.subscribe((schema: PageSchema) => {
+      this.pageSchema = schema;
+    });
+    this.selectedMaterialSubscription = this.msgService.selectedSchemaMsg.subscribe((data: any) => {
+      this.selectedSchema = data;
+    });
   }
 
+  /*
+   * @param key widget 类型
+   * @param index widget 的索引
+   */
+  output(key: string, index: number) {
+    const { data, operation, state } = this.data?.children[index]?.schema?.dataMapping[key];
+    if (this.componentStates) {
+      return this.dataMappingService.outputFromState(this.componentStates, state);
+    }
+    if (operation) {
+      return this.dataMappingService.output(operation, this.props?.dataSourceSchema, this.listItemOption);
+    }
+    return data;
+  }
+
+  trackByItems(index: number, item: any) {
+    return item.id;
+  }
+
+  insertMaterial(insertInfo: InsertInfo) {
+    this.msgService.insertWidget(insertInfo, this.data.schema.id);
+  }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { ComponentWidgetComponent } from '@/modules/shared/component-widget/component-widget.component';
 import { ContainerWidgetComponent } from '@/modules/shared/container-widget/container-widget.component';
 import { ListWidgetComponent } from '@/modules/shared/list-widget/list-widget.component';
-import { TreeWidgetComponent } from '@/modules/shared/tree-widget/tree-widget.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DndModule } from 'ngx-drag-drop';
@@ -14,19 +13,17 @@ import { DroppableDirective } from './drap-and-drop/droppable.directive';
 @NgModule({
   declarations: [
     CollapseComponent,
-    ContainerWidgetComponent,
-    TreeWidgetComponent,
     ComponentWidgetComponent,
     ListWidgetComponent,
     MaterialWrapperComponent,
     DraggableDirective,
     DroppableDirective,
+    ContainerWidgetComponent,
   ],
   imports: [CommonModule, NzButtonModule, NzIconModule, DndModule],
   exports: [
     CollapseComponent,
     ContainerWidgetComponent,
-    TreeWidgetComponent,
     ComponentWidgetComponent,
     ListWidgetComponent,
     MaterialWrapperComponent,
